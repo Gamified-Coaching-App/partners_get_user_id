3c6d3d222781346c5751d4b49d9fcb4d12b5b7e4
mariashapo <mart99maria@gmail.com>
2024-03-05 12:36:16 +0000

    Modify the test structure

--- __tests__/handler.test.mjs
+++ __tests__/handler.test.mjs
@@ -1,21 +1,30 @@
-// Importing the necessary modules for the test
 import { handler } from '../index.mjs';
 
-// Mocking AWS SDK for JavaScript v3
-jest.mock('@aws-sdk/lib-dynamodb', () => ({
+jest.mock('@aws-sdk/lib-dynamodb', () => {
+  const sendMock = jest.fn(); // Define send as a mock function at the top level
+  return {
     DynamoDBDocumentClient: {
       from: jest.fn(() => ({
-        send: jest.fn().mockResolvedValue({
-          Items: [{ user_id: 'user123' }] // Mocked success response
-        }),
-      })),
+        send: sendMock
+      }))
     },
-    QueryCommand: jest.fn()
-  }));
-  
+    QueryCommand: jest.fn(),
+    sendMock // Expose sendMock for easy access and manipulation in tests
+  };
+});
 
+// Access the sendMock directly from the mocked module for clarity
+const { sendMock } = require('@aws-sdk/lib-dynamodb');
 describe('handler function tests', () => {
+  beforeEach(() => {
+    sendMock.mockReset(); // Reset the mock to its default state before each test
+  });
+
   it('successfully fetches Garmin OAuth token and returns redirect URL', async () => {
+    sendMock.mockResolvedValue({
+      Items: [{ user_id: 'user123' }] // Mocked success response
+    });
+
     const event = {
       queryStringParameters: {
         partner_user_ids: 'partner1',
@@ -28,13 +37,14 @@ describe('handler function tests', () => {
     expect(response.statusCode).toEqual(200);
     const responseBody = JSON.parse(response.body);
     expect(responseBody).toHaveProperty('user_ids');
-    expect(responseBody.user_ids).toContain('user123'); // Expecting 'user123' to be part of the response
+    expect(responseBody.user_ids).toContain('user123');
   });
+
   it('returns error when queryStringParameters are missing', async () => {
     const event = {}; // Simulating an event without queryStringParameters
 
     const response = await handler(event);
 
-    expect(response.statusCode).toEqual(500); 
+    expect(response.statusCode).toEqual(500);
   });
 });
